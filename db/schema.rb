e73acbff1b07c2def0f16091221cc7dc2965e4f6
naoto kawai <naoto.k0407@gmail.com>
2016-09-05 11:19:19 +0000

    Make a basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831135440) do
+ActiveRecord::Schema.define(version: 20160904133226) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,13 +10,12 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
-
+  
   test "name should be present" do
-    @user.name = ""
+    @user.name = "     "
     assert_not @user.valid?
   end
-
-  test "email should be present" do
+    test "email should be present" do
     @user.email = "     "
     assert_not @user.valid?
   end
@@ -36,12 +35,20 @@ class UserTest < ActiveSupport::TestCase
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
-      test "email addresses should be unique" do
+  end
+    test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
+    test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
@@ -52,5 +59,4 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-end
 end
